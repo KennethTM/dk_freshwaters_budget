9c06a862a3920169974c275a105c25581667f443
Kenneth Thorø Martinsen <kenneth2810@gmail.com>
2020-01-23 15:04:30 +0100

    Changed upscaling to use only eq. 5 from raymond 2012. Prepared file making XYZ vector from dk dtm 10 m.

--- gis_rawdata_processing.R
+++ gis_rawdata_processing.R
@@ -16,32 +16,42 @@ rawdata_path <- paste0(getwd(), "/rawdata/")
 # 
 # saveRDS(streams, paste0(data_path, "streams.rds"))
 
+
+###
+#prepare .sqlite file for extraction of elevation values (dtm 10 m)
+#try two approaches: vector extract or rasterize/extract/vectorize - grass gis, or pktools max/min/other function?
+readRDS(paste0(data_path, "streams.rds")) %>% 
+  st_zm() %>% 
+  st_write(paste0(data_path, "streams.sqlite"))
+###
+
+
 #process stream layer to calculate slope
 streams <- readRDS(paste0(data_path, "streams.rds")) %>% 
   rownames_to_column(var = "L1") %>% 
   filter(length > 0)
 
-streams_clean <- streams %>% 
-  st_cast("MULTILINESTRING") %>% 
-  st_union() %>% 
-  st_line_merge() %>% 
-  st_cast("LINESTRING") %>% 
-  st_as_sf() %>% 
-  mutate(length = st_length(x))
-
-saveRDS(streams_clean, paste0(data_path, "streams_clean.rds"))
+# streams_clean <- streams %>% 
+#   st_cast("MULTILINESTRING") %>% 
+#   st_union() %>% 
+#   st_line_merge() %>% 
+#   st_cast("LINESTRING") %>% 
+#   st_as_sf() %>% 
+#   mutate(length = st_length(x))
+# 
+# saveRDS(streams_clean, paste0(data_path, "streams_clean.rds"))
 
 #st_cast(st_line_merge(st_union(st_cast(tmp, "MULTILINESTRING"))), "LINESTRING")
 #st_node
 #arrange by descending z values for correct order?
 #rasterize streams, extract z from dem or maybe pktools
 
-streams_slope <- streams_clean %>% 
-  #slice(1:1000) %>% 
+streams_slope <- streams %>% 
+  #slice(1:10000) %>% 
   st_coordinates() %>% 
   as.data.frame() %>% 
   #filter(L1 == 306)
-  mutate(Z_na = ifelse(Z == -999 | Z < -2, NA, Z),
+  mutate(Z_na = ifelse(Z == -999 | Z < -20, NA, Z),
          L1 = as.character(L1)) %>% 
   group_by(L1) %>% 
   #add_tally() %>%
