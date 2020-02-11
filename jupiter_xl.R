3b0bfa6706c77463c4395e2fb6c727b6653359bf
Kenneth Thorø Martinsen <kenneth2810@gmail.com>
2020-02-11 14:35:13 +0100

    Added new scripts for grw calculations. Added data for grw ph/alk and alk/calc/nitrate.

--- jupiter_xl.R
+++ jupiter_xl.R
@@ -18,79 +18,33 @@ compoundlist <- tbl(con, "COMPOUNDLIST") %>%
   select(COMPOUNDNO, LONG_TEXT) %>% 
   collect()
 
-grep("COMPOUND*", tables, value = TRUE)
+grep("long*", tables, value = TRUE)
 
 analysis <- tbl(con, "GRWCHEMANALYSIS") %>% 
   select(SAMPLEID, COMPOUNDNO, AMOUNT, UNIT)
 
 sample <- tbl(con, "GRWCHEMSAMPLE") %>% 
-  select(SAMPLEID, BOREHOLENO, SAMPLEDATE)
+  select(SAMPLEID, BOREHOLENO, TOP, BOTTOM, SAMPLEDATE)
 
 compounds <- tbl(con, "COMPOUNDLIST") %>% 
   select(COMPOUNDNO, LONG_TEXT)
   
 borehole <- tbl(con, "BOREHOLE") %>% 
-  select(BOREHOLENO, XUTM32EUREF89, YUTM32EUREF89)
+  select(BOREHOLENO, XUTM32EUREF89, YUTM32EUREF89, ELEVATION, DRILLDEPTH)
 
 all <- analysis %>% 
   left_join(sample) %>% 
   left_join(compounds) %>% 
   left_join(borehole)
 
-alk <- all %>% 
-  filter(COMPOUNDNO == 291) %>% 
+#Extract and save alk and ph data
+alk_ph <- all %>% 
+  filter(COMPOUNDNO %in% c(41, 291)) %>% 
   collect()
+saveRDS(alk_ph, paste0(getwd(), "/data/grw_ph_alk.rds"))
 
-ph <- all %>% 
-  filter(COMPOUNDNO == 41) %>% 
+#Extract and save alk, calcium and nitrate data
+alk_calc_nit <- all %>% 
+  filter(COMPOUNDNO %in% c(291, 1551, 1176)) %>% 
   collect()
-
-alk_sf <- alk %>% 
-  na.omit() %>% 
-  mutate(datetime = ymd_hms(SAMPLEDATE)) %>% 
-  filter(datetime >= ymd_hm("1990-01-01 00:00"),
-         AMOUNT > 0) %>% 
-  group_by(BOREHOLENO, XUTM32EUREF89, YUTM32EUREF89) %>% 
-  summarise(alk = mean(AMOUNT)) %>% 
-  st_as_sf(coords = c("XUTM32EUREF89", "YUTM32EUREF89"), crs = 25832)
-
-ph_sf <- ph %>% 
-  na.omit() %>% 
-  mutate(datetime = ymd_hms(SAMPLEDATE)) %>% 
-  filter(datetime >= ymd_hm("1990-01-01 00:00"),
-         between(AMOUNT, 0, 14)) %>% 
-  group_by(BOREHOLENO, XUTM32EUREF89, YUTM32EUREF89) %>% 
-  summarise(ph = mean(AMOUNT)) %>% 
-  st_as_sf(coords = c("XUTM32EUREF89", "YUTM32EUREF89"), crs = 25832)
-
-saveRDS(list(alk_sf, ph_sf), "grw_ph_alk.rds")
-
-
-
-
-library(raster)
-library(fields)
-
-dk_polygon <- getData(country = "DNK", level = 0) %>% 
-  st_as_sf() %>% 
-  st_transform(25832)
-
-dk_raster_empthy <- raster(dk_polygon, res = c(1000, 1000))
-
-alk_tps <- Tps(st_coordinates(alk_sf), alk_sf$alk)
-
-alk_raster <- interpolate(dk_raster_empthy, alk_tps)
-
-alk_raster_mask <- mask(alk_raster, as(dk_polygon, "Spatial"))
-
-ph_tps <- Tps(st_coordinates(ph_sf), ph_sf$ph)
-
-ph_raster <- interpolate(dk_raster_empthy, ph_tps)
-
-ph_raster_mask <- mask(ph_raster, as(dk_polygon, "Spatial"))
-
-library(seacarb)
-
-pco2_raster <- dk_raster_empthy
-
-pco2_raster[] <- carb(flag = 8, var1 = ph_raster_mask[], var2 = alk_raster_mask[], S = 0, T = 10)
+saveRDS(alk_calc_nit, paste0(getwd(), "/data/grw_alk_calc_nit.rds"))
